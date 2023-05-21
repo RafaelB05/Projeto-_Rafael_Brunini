0ac0c9a9fc2a69f3cce2be000f30d4c14fd81446
Rafael Brunini Pereira <bruninirafael@gmail.com>
2023-05-21 17:03:15 -0300

    Criação Classe D

---
+++ D.java
@@ -0,0 +1,13 @@
+public class D {
+    private int D1;
+    private boolean D2;
+
+    public int MD1(){
+        return D1 + 1;
+    }
+
+    public void MD2(){
+        System.out.println("MD2");
+    }
+
+}
